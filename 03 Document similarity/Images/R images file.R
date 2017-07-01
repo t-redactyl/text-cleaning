da70c3acf5a1df748ed67e5c18c4a80089a863f3
Jodie Burchell <jodie.burchell@gmail.com>
2017-07-01 11:38:46 +1000

    More changes to TF-IDF chapter

--- 03 Document similarity/Images/R images file.R
+++ 03 Document similarity/Images/R images file.R
@@ -1,9 +1,9 @@
 library(ggplot2)
 library(RColorBrewer)
-tales = read.csv("/Users/jodieburchell/Documents/text-mining/03 Document similarity/raw_frequency_sample.csv",
-                 header = TRUE)
 
 # Frequency 1 charts
+tales = read.csv("/Users/jodieburchell/Documents/text-mining/03 Document similarity/raw_frequency_sample.csv",
+                 header = TRUE)
 ggplot(tales, aes(x = term, y = frequency, fill = titles)) + 
   geom_bar(stat="identity") + 
   scale_x_discrete(name = "Term") +
@@ -14,3 +14,13 @@ ggplot(tales, aes(x = term, y = frequency, fill = titles)) +
   scale_fill_brewer(palette="Set2")
 
 # Frequency 2 charts
+tales = read.csv("/Users/jodieburchell/Documents/text-mining/03 Document similarity/prop_frequency_sample.csv",
+                 header = TRUE)
+ggplot(tales, aes(x = term, y = frequency, fill = titles)) + 
+  geom_bar(stat="identity") + 
+  scale_x_discrete(name = "Term") +
+  scale_y_continuous(name = "Proportional frequency") +
+  theme_bw() +
+  theme(legend.position="bottom", legend.direction="vertical",
+        legend.title = element_blank()) +
+  scale_fill_brewer(palette="Set2")
