25476e58debb6da98c148727c317a08e4534dbab
Jodie Burchell <jodie.burchell@gmail.com>
2017-07-01 19:01:06 +1000

    Finishing up IDF section

--- 03 Document similarity/Images/R images file.R
+++ 03 Document similarity/Images/R images file.R
@@ -4,23 +4,33 @@ library(RColorBrewer)
 # Frequency 1 charts
 tales = read.csv("/Users/jodieburchell/Documents/text-mining/03 Document similarity/raw_frequency_sample.csv",
                  header = TRUE)
-ggplot(tales, aes(x = term, y = frequency, fill = titles)) + 
-  geom_bar(stat="identity") + 
+tales$term <- factor(tales$term, 
+                     levels = c('say', 'go', 'come', 'will', 'thou', 'king', 'take',
+                                'see', 'can', 'little', 'give', 'man', 'get','away',
+                                'thee'))
+
+bcolours <- brewer.pal(8, "Set2")
+
+ggplot(tales, aes(x = term, y = frequency)) + 
+  geom_bar(stat="identity", colour = bcolours[3], fill = bcolours[3]) + 
   scale_x_discrete(name = "Term") +
-  scale_y_continuous(name = "Frequency") +
-  theme_bw() +
-  theme(legend.position="bottom", legend.direction="vertical",
-        legend.title = element_blank()) +
-  scale_fill_brewer(palette="Set2")
+  scale_y_continuous(name = "Raw frequency") +
+  theme_bw()
 
 # Frequency 2 charts
 tales = read.csv("/Users/jodieburchell/Documents/text-mining/03 Document similarity/prop_frequency_sample.csv",
                  header = TRUE)
-ggplot(tales, aes(x = term, y = frequency, fill = titles)) + 
-  geom_bar(stat="identity") + 
+
+tales$term <- factor(tales$term, 
+                     levels = c('say', 'go', 'come', 'will', 'thou', 'king', 'take', 'see',
+                                'can', 'little', 'give', 'man', 'get', 'away', 'thee'))
+
+ggplot(tales, aes(x = term, y = frequency)) + 
+  geom_bar(stat="identity", colour = bcolours[3], fill = bcolours[3]) + 
   scale_x_discrete(name = "Term") +
-  scale_y_continuous(name = "Proportional frequency") +
-  theme_bw() +
-  theme(legend.position="bottom", legend.direction="vertical",
-        legend.title = element_blank()) +
-  scale_fill_brewer(palette="Set2")
+  scale_y_continuous(name = "Sublinear term frequency") +
+  theme_bw() 
+
+# Frequency of 'say' in all tales
+
+
